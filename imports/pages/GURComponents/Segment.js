0c325feebd5265a39bf820da49fe8a7bef78c1ae
lGatl <gat55@live.fr>
2018-12-12 02:10:51 +0100

    d

--- imports/pages/GURComponents/Segment.js
+++ imports/pages/GURComponents/Segment.js
@@ -6,11 +6,11 @@ import ExampleFrame from '../../components/ExampleFrame';
 import PCode from '../../components/PCode';
 import Code from '../../components/Code';
 
-export default class AComponent extends Component {
+export default class SegmentComponent extends Component {
 	render(){
 		return (
 			<div>
-				<Titre2>A</Titre2><br/>
+				<Titre2>Segment</Titre2><br/>
 				A Segment simple to beautify <br/>
 				<Titre3>Exemple :</Titre3>
 				<ExampleFrame>
@@ -24,7 +24,7 @@ export default class AComponent extends Component {
 					<Titre3>Menu Children : </Titre3><br/>
 					Give strings or components to complete your Segment.<br/>
 				<Titre3>Optional Parameter : </Titre3><br/>
-				<Code>style</Code> : object - Change the style of this Link, it will crush the default. <br/>
+				<Code>style</Code> : object - Change the style of this Segment, it will crush the default. <br/>
 				<Code>row</Code> : boolean - Set the flex direction to row. <br/>
 				<Code>onClick</Code> : function - Give what to do when click on the Segment <br/>
 				
@@ -37,7 +37,7 @@ export default class AComponent extends Component {
 const EXEMPLE = `import React, { Component } from "react";
 import { Segment } from 'gat-ui-react'
 
-export default class AComponent extends Component {
+export default class SegmentExample extends Component {
   render(){
     return <Segment> a Segment </Segment>
   }
